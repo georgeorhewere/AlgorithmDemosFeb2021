194f8ee5cf1ff0f495653982fdbec606b94fcf38
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-02-09 07:02:48 +0100

    Add project files.

---
+++ HashTables/RansomNote.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace AlgorithmDemosFeb2021
+{
+    public class RansomNote
+    {
+        public RansomNote()
+        {
+
+        }
+
+        public  void checkMagazine(string[] magazine, string[] note)
+        {
+            HashSet<string> ransomNote = new HashSet<string>();
+
+            foreach(var word in note)
+            {
+                if (magazine.Contains(word) )
+                {
+                    ransomNote.Add(word);
+                }
+            }
+
+
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AlgorithmDemosFeb2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Algorithm Tetst");
+
+
+            Console.ReadKey();
+        }
+
+
+        public static void testRansomNote()
+        {
+            string[] magazine = new string[] { "give", "me", "one", "grand", "today", "night" };
+            string[] note = new string[] { };
+
+        }
+    }
+}
